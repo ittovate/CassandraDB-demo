4c261788c76185373a5f59cbdfabc95d1c24f974
mustafa-abuelmagd <mustafa.2buelmagd@gmail.com>
2024-07-10 08:27:43 +0300

    update Folder repository to use Cassandra repository

--- src/main/java/com/example/cassandradbdemo/repos/FolderRepository.java
+++ src/main/java/com/example/cassandradbdemo/repos/FolderRepository.java
@@ -1,13 +1,13 @@
 package com.example.cassandradbdemo.repos;
 
 import com.example.cassandradbdemo.models.Folder;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface FolderRepository extends CrudRepository<Folder, String> {
+public interface FolderRepository extends CassandraRepository<Folder, String> {
     List<Folder> findAllById(String id);
 }
